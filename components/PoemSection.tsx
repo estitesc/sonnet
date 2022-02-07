e2033ad29de8e86be82bd5cb32abb56502da8c9f
Evan Stites-Clayton <stitesclayton@gmail.com>
2022-02-07 12:56:12 -0800

    Adds poem display screen with options for look and feel

---
+++ components/PoemSection.tsx
@@ -0,0 +1,103 @@
+import * as React from 'react';
+import useIsDesktop from '../h/useIsDesktop';
+import Image from "next/image";
+
+interface PoemSectionProps {
+  poem: any;
+}
+
+const PoemSection: React.FC<PoemSectionProps> = ({poem}) => {
+    const isDesktop = useIsDesktop();
+
+    // 8.5 22- for desktop
+    // 6.4, 9, 19.5- worked well
+    const getFontSize = (poemSize: number) => {
+      if(isDesktop) {
+        if(poemSize === 14) {
+          return 16;
+        }
+        else {
+          return 30 - poemSize;
+        }
+      } else {
+        if(poemSize === 14) {
+          return 6.4;
+        }
+        if(poemSize === 10) {
+          return 9;
+        }
+        else {
+          return 19.5 - poemSize;
+        }
+      }
+    }
+
+    return (
+      <div style={{width: '100%', display: 'flex', justifyContent: 'center'}}>
+        <div>
+          <div style={{fontSize: 12, marginBottom: 24}}>
+            SONN3T #1124
+          </div>
+          <div 
+            id="content" 
+            style={{
+              display: 'flex',
+              flexDirection:'column',
+              fontSize: getFontSize(poem.size),
+              lineHeight: '125%',
+            }}>
+              { poem.lines.map((line: any, key: string) => {
+                return(
+                  <div key={key} style={{whiteSpace: 'pre'}}>{line}</div>
+                )
+              })}
+          </div>
+          <div style={{fontSize: 12, marginTop: 24}}>
+            09.23.22
+          </div>
+          {/* <div style={{display: 'flex', alignItems: 'center' , marginTop: 24}}>
+            <div style={{height: 28, width: 28, overflow: 'hidden', borderRadius: 12, marginRight: 12}}>
+              <Image src="/disclaimed_witch.png" height={28} width={28} />
+            </div>
+            <div>
+            <div style={{fontSize: 12}}>
+              marca.soli
+            </div>
+            <div style={{fontSize: 12, lineHeight: 1}}>
+            09.23.22
+          </div>
+
+            </div>
+            
+            </div> */}
+          
+          <div style={{fontSize: 12, marginTop: 24}}>
+            <b>poet</b> marca.soli
+          </div>
+          <div style={{fontSize: 12}}>
+            <b>patron</b> charli0.eth
+          </div>
+          <div style={{display: 'flex', alignItems: 'baseline'}}>
+          <div style={{fontSize: 12, marginTop: 4}}>
+            <b>last</b> ⧫ 2.2
+          </div>
+          <div style={{display: 'inline-block', marginLeft: 12}}>
+                <div style={{ 
+                  color: '#0B0705', 
+                  backgroundColor: '#FDFCFC', 
+                  padding: 4,
+                  borderRadius: 4,
+                  fontSize: 10,
+                  lineHeight: 1,
+                  fontWeight: 'bold'
+                  }}>make offer</div>
+              </div>
+          </div>
+          
+        </div>
+      </div>
+      
+    );
+}
+
+export default PoemSection;
---
+++ pages/poem/[poemId].jsx
@@ -0,0 +1,66 @@
+import * as React from 'react';
+import { useRouter } from 'next/router';
+import Navbar from '../../components/NavbarNew';
+import PoemSection from '../../components/PoemSection';
+import styles from '../../styles/Home.module.css';
+
+const Poem = ({props}) => {
+  console.log("props are", props);
+
+  const router = useRouter();
+  const { poemId } = router.query;
+
+  const poemLines12 = [
+    "sunny chillshine in a bar",
+    "nodding off - window seat",
+    "putting together words of",
+    "things we wanted to lose",
+    "the holy sin shine of mew",
+    "the undiscoverable dread",
+    "that swell picadilly hall",
+    "sunny chillshine in a bar",
+    "children new muse easy",
+    "so there was still some",
+    "sunny chillshine in a bar",
+    "and yet we faltered.",
+  ];
+
+  const poemLines14 = [
+    "lost in a creepy conversation",
+    "not knowing who I am      now",
+    "they speak of influencers and",
+    "nft marketing, while   others",
+    "here sip kava and craft  love",
+    "a girl hums happy, nonchalant",
+    "lost in a creepy conversation",
+    "not knowing who I am      now",
+    "they speak of influencers and",
+    "nft marketing, while   others",
+    "here sip kava and craft  love",
+    "a girl hums happy, nonchalant",
+    "here sip kava and craft  love",
+    "a girl hums happy, nonchalant",
+  ];
+
+  const poem12 = {
+    size: 12,
+    lines: poemLines12,
+    }
+
+  const poem14 = {
+      size: 14,
+      lines: poemLines14,
+  }
+
+  const poems = [poem12, poem14];
+  const poem = poems[poemId] || poem12;
+
+  return (
+    <div className={styles.container}>
+      <Navbar account={"0x5ome1…"} poemCount={1} poemId={1} />
+      <PoemSection poem={poem}/>
+    </div>
+  )
+}
+
+export default Poem;
